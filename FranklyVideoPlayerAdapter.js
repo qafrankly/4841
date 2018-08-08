a462770956781f49f351f2b8430d674c291052a4
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-08-08 18:56:44 -0400

    Update FranklyVideoPlayerAdapter.js

--- FranklyVideoPlayerAdapter.js
+++ FranklyVideoPlayerAdapter.js
@@ -1,80 +1,84 @@
 import React, { Component, PropTypes } from 'react';
-import Video from 'components/media/Video'; //clearly only exists on Frankly servers
-
-// const wNVideoCanvasPropsExample = [{
-//   SetFlashLiveStream: [{
-//     strUrl: url,
-//     strHeadline: 'Bike Video (1)',
-//     strAdTag: 'Weather - Live Streaming',
-//     hasPreroll: true,
-//     strSummaryImageUrl: '',
-//     mobileStreams: [{
-//       url,
-//       type: 'video/mp4'
-//     }, {
-//       url: '',
-//       type: 'video/mp4'
-//     }, {
-//       url: '',
-//       type: 'video/mp4'
-//     }]
-//   }]
-// }];
+import Video from 'components/media/Video';
 
 class FranklyVideoPlayerAdapter extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      autoplay: props.autoPlay || false,
-      controls: true,
-      sources:props.sources || [{src: false, type: false}],
-      poster: props.poster || '',
-      showAdContainer: true,
+      video: ''
+    };
+    
+    this.updateVideo = this.updateVideo.bind(this);
 
-    }
   }
 
-  propsAdapter = ()=> {
-    let mobileStreams = [{
-       url: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
-       type: 'video/mp4'
-     }, {
-       url: '',
-       type: 'video/mp4'
-     }, {
-       url: '',
-       type: 'video/mp4'
-     }];
-
-    let franklyProps = {
-      clipId: 1,
-      autoPlay: true,
-      wNVideoCanvasProps: [{
-        SetFlashLiveStream: [{
-          strUrl: 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8',
-          strHeadline: '',
-          strAdTag: this.props.contentClass,
-          hasPreroll: !!this.props.contentClass,
-          strSummaryImageUrl: '',
-          mobileStreams: mobileStreams
+  componentDidMount() { // set initial video
+    const url = 'https://content.jwplatform.com/manifests/yp34SRmf.m3u8';
+    const wNVideoCanvasProps = [{
+      SetFlashLiveStream: [{
+        strUrl: url,
+        strHeadline: 'Bike Video (1)',
+        strAdTag: 'Weather - Live Streaming',
+        hasPreroll: true,
+        strSummaryImageUrl: '',
+        mobileStreams: [{
+          url,
+          type: 'video/mp4'
+        }, {
+          url: '',
+          type: 'video/mp4'
+        }, {
+          url: '',
+          type: 'video/mp4'
         }]
       }]
-    }
-
-    console.log('franklyProps', franklyProps)
-    return franklyProps
+    }];
+      
+    this.setState({
+      video: <Video clipId={1} autoPlay={true} wNVideoCanvasProps={wNVideoCanvasProps} />
+    });
   }
+  
+  async updateVideo() {
+    
+    const url = 'http://d2zihajmogu5jn.cloudfront.net/bipbop-advanced/bipbop_16x9_variant.m3u8';
+    const wNVideoCanvasProps = [{
+      SetFlashLiveStream: [{
+        strUrl: url,
+        strHeadline: 'Test Video (2)',
+        strAdTag: 'Weather - Live Streaming',
+        hasPreroll: true,
+        strSummaryImageUrl: '',
+        mobileStreams: [{
+          url,
+          type: 'video/mp4'
+        }, {
+          url: '',
+          type: 'video/mp4'
+        }, {
+          url: '',
+          type: 'video/mp4'
+        }]
+      }]
+    }];
+    
+    await this.setState({ video: '' }); // clear the current video before setting a new one.
 
-
-
-
+    // clipId is required if you are not using FRN_rawResponses
+    this.setState({
+      video: <Video clipId={2} autoPlay={true} wNVideoCanvasProps={wNVideoCanvasProps} />  
+    });
+  }
+  
   render(){
     return (
-      <div className='frankly-video-player-adapter'>
-	      <Video {...this.propsAdapter()} />
+      <div className='VideoModuleDemo'>	
+        <h1>Video Demo</h1> 
+        <button onClick={this.updateVideo}>Video2</button>
+	      {this.state.video}
       </div>
     );
-
+	  
   }
 }
 
